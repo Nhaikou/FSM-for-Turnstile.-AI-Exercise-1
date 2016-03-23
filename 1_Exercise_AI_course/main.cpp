4ef1b6aa465fbf0003d3d1a5d31ca25ea2cdc5c1
Nhaikou <nikokinnunen92@gmail.com>
2016-03-23 13:48:56 +0200

    initial commit

---
+++ 1_Exercise_AI_course/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+class Turnstile
+{
+public:
+	Turnstile(){};
+	~Turnstile(){};
+
+private:
+
+};
+
+class TurnstileFSM
+{
+public:
+	TurnstileFSM(){};
+	~TurnstileFSM(){};
+
+private:
+
+};
+
+class TurnstileState
+{
+public:
+	TurnstileState(){};
+	~TurnstileState(){};
+
+private:
+
+};
+
+
+
+void main()
+{
+
+}
